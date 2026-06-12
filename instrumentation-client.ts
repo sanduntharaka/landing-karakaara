42cf359dd89b2efc3eb8be7a1f615eb88928457f
Sandun Tharaka <67332822+sanduntharaka@users.noreply.github.com>
2026-06-12 08:24:21 +0530

    removed posthot

--- instrumentation-client.ts
+++
@@ -1,7 +0,0 @@
-import posthog from "posthog-js";
-
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "/ingest",
-  ui_host: "https://us.posthog.com",
-  defaults: "2026-05-30",
-});
